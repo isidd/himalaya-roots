1c629f20daa25c81d3e4497a039a6e8f353afd60
isidd <pharasi.siddhartha@gmail.com>
2024-01-16 19:19:41 +0530

    philosophy text added

--- src/components/pageRack/landingPageStack/philosophy/index.tsx
+++ src/components/pageRack/landingPageStack/philosophy/index.tsx
@@ -5,9 +5,10 @@ import sig from "@/assets/pictures/sig.svg";
 export default function Philosophy(){
         return (
             <Container maxWidth="xl" sx={(theme)=>({background:theme.palette.primary.main,color:"#fff",padding:"40px 0 "})} >
-                <Box sx={{padding : "90px 70px"}} >
+                    <Typography variant="h3" mb={3} textAlign={"center"} >Our Philosophy</Typography>
+                <Box sx={{padding : "70px 70px"}} >
                     <Typography variant="h5" textAlign={"center"} >
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas dolorem odit ratione natus praesentium atque labore, nostrum magni molestiae porro dolor corporis ea repudiandae quo et, nam repellat! Dolore, voluptatum.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos itaque, cupiditate libero eius quaerat voluptates natus quasi reiciendis sequi. A vero ex assumenda, veritatis officiis deserunt iste! Eum, repellendus autem debitis tempora cupiditate pariatur tenetur consequuntur assumenda, quo veritatis voluptatibus, tempore labore odit doloremque itaque fuga minus aut sed temporibus amet veniam voluptates repudiandae quia laudantium! Incidunt doloribus aliquam magni?
                     </Typography>
                 </Box>
                 <Box mb={"30px"} textAlign={"center"} >
